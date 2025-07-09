5682764566a79bbd29436efdf1a7ebd392d895ca
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-09 09:33:28 -0300

    Exercício 02, correção e adição de funcionalidade

--- Exercício02.cpp
+++ Exercício02.cpp
@@ -1,16 +1,24 @@
 // Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.
 
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
+    double ladoA;
 
-    int ladoA = 5;
-    int A = ladoA * ladoA;
-    int P = ladoA * 4;
+    std::cout << "Digite o valor do lado do quadrado: ";
+    std::cin >> ladoA; // lê lado do usuário
 
-    std::cout << A << '\n';
-    std::cout << P << '\n';
+    double A = ladoA * ladoA;
+    double P = 4 * ladoA;
+
+    // Controle de casas decimais
+    std::cout << std::fixed << std::setprecision(2);
+
+    std::cout << "Dados o lado = " << ladoA
+              << " , a área é " << A << " cm\u00B2"
+              << " e o perímetro é " << P << " cm\n";
 
     return 0;
 }
