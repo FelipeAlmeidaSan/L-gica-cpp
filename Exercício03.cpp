0f98136423777ae1cc3d37f1f909839b11435c28
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-16 08:33:11 -0300

    commit test

---
+++ Exercício03.cpp
@@ -0,0 +1,10 @@
+// Dado o tamanho do raio de uma circunferência, calcular a área e o perímetro da mesma.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
