39da3eeb734b82ebf410da27588628af3b28e9d9
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-16 09:04:56 -0300

    Primeira versão do exercício 03

--- Exercício03.cpp
+++ Exercício03.cpp
@@ -1,10 +1,22 @@
 // Dado o tamanho do raio de uma circunferência, calcular a área e o perímetro da mesma.
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+    double raio;
+    cout << "Digite o valor do raio: ";
+    cin >> raio;
+
+    const double pi = 3.1415;
+
+        int A = pi * raio * raio;
+    cout << "O valor da área dado o raio " << raio << " é de: " << A << '\n';
+
+    double P = 2.0 * pi * raio;
+    cout << "O valor do perímetro é igual à " << P << '\n';
 
     return 0;
 }
